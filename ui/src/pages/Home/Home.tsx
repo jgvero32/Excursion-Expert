9942d4ba36eb931e8c356d5a605df9504aadcde8
jgvero32 <jgvero32@gmail.com>
2024-10-31 15:33:37 -0500

    Modify Home page links to correctly route to login and register

--- ui/src/pages/Home/Home.tsx
+++ ui/src/pages/Home/Home.tsx
@@ -2,8 +2,8 @@ export function Home() {
   return(
     <>
       <h1>Home</h1>
-      <a href="/users/register">Register</a>
-      <a href="/users/login">Login</a>
+      <a href="/register">Register</a>
+      <a href="/login">Login</a>
     </>
   );
 }
