c810943ba2c3e70da1f5813d8a73799017b99dbe
acruz <acruz@vailsys.com>
2024-11-11 21:50:01 -0600

    Added Profile and text for About us

--- ui/src/pages/AboutUs/AboutUs.tsx
+++ ui/src/pages/AboutUs/AboutUs.tsx
@@ -5,10 +5,10 @@ export function AboutUs() {
     <div className="aboutUs">
       <Typography className="aboutUs__title">About Us</Typography>
       <Typography className="aboutUs__content">
-        What is Lorem Ipsum?<br />
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br />
-        Why do we use it?<br />
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        What is Excursion Expert?<br />
+        Welcome to Excursion Expert – your ultimate companion for creating unforgettable travel itineraries! <br /><br />
+        We know that planning the perfect trip can be overwhelming, with so many options to explore and details to consider. That’s why we’re here to take the guesswork out of your travel plans, offering a smart, guided way to craft personalized itineraries, no matter where your mind wants to take you. <br /><br />
+        At Excursion Expert, we believe travel should be about exploration, not stress. Whether you’re a seasoned adventurer or a first-time traveler, our platform combines cutting-edge technology with expert recommendations to make planning your trip both easy and exciting. With curated destinations, local insights, and customizable schedules, we ensure that every step of your journey is tailored to your unique preferences.
       </Typography>
     </div>
   );
--- ui/src/pages/Profile/Profile.tsx
+++ ui/src/pages/Profile/Profile.tsx
@@ -1,5 +1,51 @@
-export const Profile = () => {
-  return(
-    <>Profiles</>
+import { Box, Button, TextField, Typography} from '@mui/material';
+import './Profile.scss';
+
+export function Profile() {
+
+
+  return (
+    <Box className="profile">
+      <Box className="profile-container">
+        <Typography variant="h4" className="profile-title">
+          Your Profile
+        </Typography>
+        <Box component="form" className="profile-form">
+          <Box className="profile-field">
+            <TextField
+              sx={{backgroundColor:"White", borderRadius: "8px"}}
+              label="Username"
+              name="username"
+              fullWidth
+            />
+          </Box>
+          <Box className="profile-field">
+            <TextField
+              sx={{backgroundColor:"White", borderRadius: "8px"}}
+              label="Password"
+              name="password"
+              fullWidth
+            />
+          </Box>
+          <Box className="profile-field">
+            <TextField 
+              sx={{backgroundColor:"White",borderRadius: "8px"}}
+              label="Default Location"
+              name="location"
+              fullWidth
+            />
+          </Box>
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            className="update-button"
+            sx={{ marginTop: 2 }}
+          >
+            Update Profile!
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 }
