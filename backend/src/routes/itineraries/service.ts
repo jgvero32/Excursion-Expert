294b8529054d5d18ebedec9f592ffd457396e2d5
dmroz5 <91923941+dmroz5@users.noreply.github.com>
2024-11-11 20:27:28 -0600

    added logic for database population

--- backend/src/routes/itineraries/service.ts
+++ backend/src/routes/itineraries/service.ts
@@ -1,6 +1,8 @@
 import { UpdateItineraryRequest } from './models';
 import { v4 as uuidv4 } from 'uuid';
 
+const { pool } = require("../../../dbConfig");
+
 export class ItineraryService {
   static async saveItinerary(requestBody: UpdateItineraryRequest): Promise<string> {
     console.log('saveItinerary', requestBody);
@@ -22,6 +24,43 @@ export class ItineraryService {
     // }
 
     const itineraryId = uuidv4(); // this is how to generate a unique uuid id
+    const iter_add = [
+      itineraryId,
+      itinerary.city,
+      itinerary.itineraryName,
+      itinerary.username
+    ];
+    try{
+      const result = await pool.query("INSERT INTO itineraries VALUES ($1, $2, $3, $4) RETURNING *", iter_add); //creates itinerary and adds to table
+      console.log("inserted itinerary: ", result.rows[0])
+    } catch (error) {
+        console.error("Error inserting itinerary:", error);
+    }
+    for (const p of itinerary.places){
+      const curr = [
+        p.name,
+        itinerary.city,
+        "https://maps.google.com/",
+        p.rating,
+        itineraryId
+      ]
+      try{
+        const place = await pool.query("INSERT INTO landmarks VALUES ($1, $2, $3, $4, $5) RETURNING *", curr); //adds each landmark per itinerary
+      } catch (error) {
+          console.error("Error inserting landmark:", error);
+      }
+      for (const type of p.types){
+        const currtype = [
+          p.name,
+          type
+        ] 
+        try{
+          const ret_type = await pool.query("INSERT INTO landmark_type VALUES ($1, $2) RETURNING *", currtype); //adds each landmark type per landmark
+        } catch (error) {
+            console.error("Error inserting type:", error);
+        }
+      }
+    }
     return itineraryId;
   }
 }
